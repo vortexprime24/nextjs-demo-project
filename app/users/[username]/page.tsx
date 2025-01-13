a08bb4787620e27a083b9d4b51a58ac8ea386178
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-01-13 19:23:53 +0530

    build error x3

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -1,12 +1,13 @@
 import CurrentUser from "@/components/CurrentUser";
-export default async function UserPage({
-  params,
-}: {
-  params: { username: string };
-}) {
+
+type Params = Promise<{ username: string }>;
+
+export default async function UserPage(props: { params: Params }) {
+  const params = await props.params;
+  const username = params.username;
   return (
     <header className="w-screen h-[400px] bg-blue-500 flex items-center justify-center">
-      <CurrentUser username={params.username} />
+      <CurrentUser username={username} />
     </header>
   );
 }
