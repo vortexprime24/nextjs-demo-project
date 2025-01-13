3fac32f6d336a9680974332cdcc22ba522e8f1cd
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-01-13 19:20:40 +0530

    build errors x2

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -1,5 +1,9 @@
 import CurrentUser from "@/components/CurrentUser";
-export default function UserPage({ params }: { params: { username: string } }) {
+export default async function UserPage({
+  params,
+}: {
+  params: { username: string };
+}) {
   return (
     <header className="w-screen h-[400px] bg-blue-500 flex items-center justify-center">
       <CurrentUser username={params.username} />
